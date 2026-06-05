c220a2461eb753579ded0c90b6caad65de630823
Pyae Sone <pyaesone.perfect2014@gmail.com>
2026-06-05 10:27:19 +0800

    Add Chroma guideline chunking pipeline

--- data/chroma/chunk.py
+++ data/chroma/chunk.py
@@ -1,10 +1,36 @@
-import pymupdf
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+import chromadb
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-doc = pymupdf.open("data/guidelines/5506cpg1.pdf")
-out = open("output.txt", "wb")
+folder_path = Path("data/guidelines")
 
-for page in doc:
-    text = page.get_text().encode("utf-8")
-    out.write(text)
-    out.write(bytes((12,)))
-out.close()
+loaders = []
+
+for file in folder_path.iterdir():
+    if file.is_file() and file.suffix.lower() == ".pdf":
+        loader = PyPDFLoader(str(file))
+        loaders.append(loader)
+
+docs = []
+
+for loader in loaders:
+    docs.extend(loader.load())
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+
+splits = text_splitter.split_documents(docs)
+
+client = chromadb.PersistentClient(path="data/chroma/chroma_db")
+collection = client.get_or_create_collection(name="guidelines")
+
+
+collection.add(
+    documents=[split.page_content for split in splits],
+    ids=[f"{split.metadata['source']} : {split.metadata['page']} : chunk_index_{i}" for i, split in enumerate(splits)],
+    metadatas=[{
+        "source": split.metadata["source"],
+        "page_number": split.metadata["page"]
+    } for split in splits]
+)
+    
